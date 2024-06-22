982540214a9458c89c32a8005420001e0711df01
Jonas List <Jonas.List.1289@gmail.com>
2024-06-22 18:11:48 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/portfolio",
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
