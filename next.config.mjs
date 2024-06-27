5b8f8f7e80edf5c8c0171deedb23c801c37bade6
Jonas List <Jonas.List.1289@gmail.com>
2024-06-27 19:18:44 +0200

    Fixed marquee

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/portfolio",
-  output: "export",
-  distDir: 'out',
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
+
 };
 
 export default nextConfig;
