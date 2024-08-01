a977187566303f7d293e45d7199bada6a62d93b3
Jonas List <Jonas.List.1289@gmail.com>
2024-08-02 00:24:47 +0200

    Update sitemap and robots files

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,9 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [{
     url: `${process.env.NEXT_PUBLIC_SITE_URL}/`,
+    priority: 1,
   }, ...localesAsStrings.map(locale => ({
     url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/`,
+    priority: 0.8,
   })).flat()];
 }
