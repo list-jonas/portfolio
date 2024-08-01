8bae7cf52f34383f8d1d477a31215143b070177c
Jonas List <Jonas.List.1289@gmail.com>
2024-08-01 21:09:41 +0200

    Update language-specific URLs in sitemap.xml

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { Suspense } from "react";
-import Loading from "../loading";
+import Loading from "./loading";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import localfont from "next/font/local";
