fc48467996be085e20a33320b06cb7fb840c3331
Jonas List <Jonas.List.1289@gmail.com>
2024-08-09 19:54:51 +0200

    Update layout.tsx to include HTML structure and meta tags

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -14,6 +14,7 @@ const generalSans = localfont({
       path: "../../../public/fonts/GeneralSans-Variable.ttf",
     },
   ],
+
   variable: "--font-general-sans",
 });
 
@@ -50,24 +51,89 @@ export default async function RootLayout({
   const messages = await getMessages();
 
   return (
-    <body style={{ height: "100%" }} className={generalSans.className}>
-      <Analytics />
-      <SpeedInsights />
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <NextIntlClientProvider messages={messages}>
-          <div className="relative">
-            <Navigation />
-            <main className="relative flex justify-center top-36 overflow-x-hidden">
-              {children}
-            </main>
-          </div>
-        </NextIntlClientProvider>
-      </ThemeProvider>
-    </body>
+    <html lang={locale}>
+      <head>
+        {/* Structured Data */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Person",
+              name: "Jonas List",
+              jobTitle: "Freelancer",
+              description: "Freelancer – Selbstständig",
+              address: {
+                "@type": "PostalAddress",
+                addressLocality: "Hitzendorf",
+                addressRegion: "Styria",
+                addressCountry: "Austria",
+              },
+              url: "https://jonas-list.vercel.app/",
+              sameAs: [
+                "https://about.me/jonas.list",
+                "https://www.linkedin.com/in/jonas-list-740230289/",
+                "https://github.com/list-jonas",
+                "https://orcid.org/0009-0006-2584-1078",
+              ],
+              image: "https://jonas-list.vercel.app/images/profile-image.png",
+              email: "mailto:jonas.list.1289@gmail.com",
+              alumniOf: {
+                "@type": "EducationalOrganization",
+                name: "HTBLA Kaindorf",
+              },
+              birthDate: "2005-10-08",
+              knowsAbout: ["Software Engineering", "Web Development", "Design"],
+            }),
+          }}
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
+      <body style={{ height: "100%" }} className={generalSans.className}>
+        <Analytics />
+        <SpeedInsights />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NextIntlClientProvider messages={messages}>
+            <div className="relative">
+              <Navigation />
+              <main className="relative flex justify-center top-36 overflow-x-hidden">
+                {children}
+              </main>
+            </div>
+          </NextIntlClientProvider>
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
