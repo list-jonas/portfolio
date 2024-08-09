872c520c73e6840d9bd0a92186c710f30c51631c
Jonas List <Jonas.List.1289@gmail.com>
2024-08-09 20:00:00 +0200

    Update CoffeeButton component to improve accessibility and add tooltip for Buy Me a Coffee link

--- src/components/nav/coffee-button.tsx
+++ src/components/nav/coffee-button.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import { Button } from '../ui/button';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip';
-import { Coffee } from 'lucide-react';
-import Link from 'next/link';
-import { useTranslations } from 'next-intl';
-import { cn } from '@/lib/utils';
+import { cn } from "@/lib/utils";
+import { Coffee } from "lucide-react";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
 
 interface CoffeeButtonProps {
   className?: string;
 }
 
 const CoffeeButton = ({ className }: CoffeeButtonProps) => {
-  const t = useTranslations('Nav');
+  const t = useTranslations("Nav");
 
   return (
     <TooltipProvider disableHoverableContent>
       <Tooltip delayDuration={100}>
         <TooltipTrigger asChild>
-          <Button variant="outline" size="icon" className={cn("font-normal", className)} asChild>
-            <Link href={"https://buymeacoffee.com/jonasl"} target='blank' ><Coffee /></Link>
+          <Button
+            variant="outline"
+            size="icon"
+            className={cn("font-normal", className)}
+            asChild
+          >
+            <Link
+              href={"https://buymeacoffee.com/jonasl"}
+              target="blank"
+              aria-label="Buy me a coffee"
+            >
+              <Coffee />
+            </Link>
           </Button>
         </TooltipTrigger>
         <TooltipContent side="bottom">{t("coffeeTooltip")}</TooltipContent>
