b2c5db19d0006edefe3b412cd4023ed12f9833d2
Jonas List <Jonas.List.1289@gmail.com>
2024-08-19 15:22:26 +0200

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,7 +8,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: `${process.env.NEXT_PUBLIC_SITE_URL}/`,
       priority: 1,
-      lastmod: today,  // Add lastmod
+      lastModified: today,  // Add lastmod
       alternateRefs: localesAsStrings.map((locale) => ({
         href: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/`,
         hreflang: locale,
@@ -17,7 +17,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     ...localesAsStrings.map((locale) => ({
       url: `${process.env.NEXT_PUBLIC_SITE_URL}/${locale}/`,
       priority: 0.8,
-      lastmod: today,  // Add lastmod
+      lastModified: today,  // Add lastmod
       alternateRefs: localesAsStrings.map((altLocale) => ({
         href: `${process.env.NEXT_PUBLIC_SITE_URL}/${altLocale}/`,
         hreflang: altLocale,
