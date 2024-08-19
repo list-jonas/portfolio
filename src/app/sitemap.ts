f017b3e1942f1b3e43391c32c352f9d8470fd6ae
Jonas List <Jonas.List.1289@gmail.com>
2024-08-19 20:22:16 +0200

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,28 +2,19 @@ import { localesAsStrings } from "@/lib/locales";
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const hardcodedDate = '2024-08-19';  // Set today's date manually
+  const today = "2024-08-19"; // Manually set date
 
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL; // Base URL from environment
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
 
   return [
     {
       url: `${baseUrl}/`,
-      lastModified: new Date(hardcodedDate),  // Use manually set date
+      lastModified: today,
       alternates: {
         languages: Object.fromEntries(
           localesAsStrings.map((locale) => [locale, `${baseUrl}/${locale}/`])
         ),
       },
     },
-    ...localesAsStrings.map((locale) => ({
-      url: `${baseUrl}/${locale}/`,
-      lastModified: new Date(hardcodedDate),  // Manually set date
-      alternates: {
-        languages: Object.fromEntries(
-          localesAsStrings.map((altLocale) => [altLocale, `${baseUrl}/${altLocale}/`])
-        ),
-      },
-    })),
   ];
 }
