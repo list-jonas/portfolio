68944faeac629e6c41862ee977c3a863d61d0393
Jonas List <Jonas.List.1289@gmail.com>
2024-09-13 13:55:35 +0200

    Add mobile responsiveness to ParticlesOverlay component

--- src/components/particle-overlay.tsx
+++ src/components/particle-overlay.tsx
@@ -8,13 +8,14 @@ import Particles from "@/components/magicui/particles";
 const ParticlesOverlay = () => {
   const { theme } = useTheme();
   const [color, setColor] = useState("#b6b6b6");
+  const mobile = window.innerWidth < 640;
 
   useEffect(() => {
     setColor(theme === "dark" ? "#b6b6b6" : "#171819");
   }, [theme]);
 
-  return (
-    <div className="h-full w-full -z-10 max-sm:hidden">
+  return mobile ? undefined : (
+    <div className="h-full w-full -z-10">
       <Particles
         className="absolute inset-0"
         quantity={100}
